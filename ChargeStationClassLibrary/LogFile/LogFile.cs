32002cbe459c15533ed1e832c4da70a52f55e3d5
Christina Jørgensen <au669829@uni.au.dk>
2022-03-17 12:53:43 +0100

    DTO klasse tilføjet

--- ChargeStationClassLibrary/LogFile/LogFile.cs
+++ ChargeStationClassLibrary/LogFile/LogFile.cs
@@ -8,34 +8,27 @@ namespace ChargeStationClassLibrary.LogFile
 {
     public class LogFile : ILogFile
     {
-        public int Id { get; set; }
-        public bool Locked { get; set; }
-        public string Description { get; set; }
-        public DateTime TimeStamp { get; set; }
+        private DTO_LogData dtoLogData;
 
-        LogFileSerialize logFileSerialize;
-
-        public LogFile()
+        public LogFile(DTO_LogData dtoLog)
         {
-            
+            dtoLogData = dtoLog;
         }
 
         public void LogDoorLocked(int id)
         {
-            Id = id;
-            Locked = true;
-            //timestamp (timer, min, sek)
-            Description = "Door is locked";
-            TimeStamp = DateTime.Now;
+            dtoLogData.Id = id;
+            dtoLogData.Locked = true;
+            dtoLogData.Description = "Door is locked";
+            dtoLogData.TimeStamp = DateTime.Now;
         }
 
         public void LogDoorUnlocked(int id)
         {
-            Id = id;
-            Locked = false;
-            //timestamp (timer, min, sek)
-            Description = "Door is unlocked";
-            TimeStamp = DateTime.Now;
+            dtoLogData.Id = id;
+            dtoLogData.Locked = false;
+            dtoLogData.Description = "Door is unlocked";
+            dtoLogData.TimeStamp = DateTime.Now;
         }
     }
 }
