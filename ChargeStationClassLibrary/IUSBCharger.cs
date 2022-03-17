dc23b2392702e629ae9d887056d6a1063b66a2c9
Gustav Elbrønd <gustav.elbroend@gmail.com>
2022-03-17 13:09:49 +0100

    Tilføjelser af metoder

--- ChargeStationClassLibrary/IUSBCharger.cs
+++ ChargeStationClassLibrary/IUSBCharger.cs
@@ -8,8 +8,19 @@ namespace ChargeStationClassLibrary
 {
     public interface IUSBCharger
     {
-        public event EventHandler<CurrentChangedEventArgs> CurrentChangedEvent;
+        // Event triggered on new current value
+        event EventHandler<CurrentChangedEventArgs> CurrentChangedEvent;
 
+        // Direct access to the current current value
+        double CurrentValue { get; }
+
+        // Require connection status of the phone
+        bool Connected { get; }
+
+        // Start charging
+        void StartCharge();
+        // Stop charging
+        void StopCharge();
 
     }
 }
