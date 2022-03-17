2e6bae0f19ba4f47c3babcdc014c3105572367d8
NadiaAJensen <78858892+NadiaAJensen@users.noreply.github.com>
2022-03-17 12:33:53 +0100

    StationControl klassen er oprettet

--- ChargeStationClassLibrary/Door/DoorChangedEventArgs.cs
+++ ChargeStationClassLibrary/Door/DoorChangedEventArgs.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ChargeStationClassLibrary
+namespace ChargeStationClassLibrary.Door
 {
     public class DoorChangedEventArgs: EventArgs
     {
---
+++ ChargeStationClassLibrary/StationControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargeStationClassLibrary
+{
+    public class StationControl
+    {
+        public void DoorOpened()
+        {
+
+        }
+
+        public void DoorClosed()
+        {
+
+        }
+
+        private void RfidDetected(int id)
+        {
+
+        }
+
+        public void CheckID(int oldId, int id)
+        {
+
+        }
+
+        private void HandleDoorChangedEvents()
+        {
+
+        }
+    }
+}
