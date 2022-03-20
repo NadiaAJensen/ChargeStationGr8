d3cb62a0ed78ed4bb7b0e66fa24c20601d3891b1
NadiaAJensen <78858892+NadiaAJensen@users.noreply.github.com>
2022-03-20 21:02:36 +0100

    TestDoor oprettet

---
+++ ChargeStationUnitTests/TestDoor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ChargeStationClassLibrary.Door;
+
+namespace ChargeStationUnitTests
+{
+    class TestDoor
+    {
+        private Door _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Door();
+        }
+
+        [Test]
+        public void 
+    }
+}
