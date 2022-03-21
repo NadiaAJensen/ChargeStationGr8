283b37fd1103ce72458994adba0dea435f211b11
NadiaAJensen <78858892+NadiaAJensen@users.noreply.github.com>
2022-03-21 22:22:14 +0100

    DoorTest færdigjort

--- ChargeStationUnitTests/TestDoor.cs
+++ ChargeStationUnitTests/TestDoor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,50 @@ namespace ChargeStationUnitTests
     class TestDoor
     {
         private Door _uut;
+        private DoorChangedEventArgs _receivedEventArgs;
 
         [SetUp]
         public void Setup()
         {
+            _receivedEventArgs = null; 
             _uut = new Door();
+
+            _uut.DoorStatusChangedEvent += (o, args) => _receivedEventArgs = args;
         }
 
         [Test]
-        public void 
+        public void TestUnLockDoor()
+        {
+            var stringwriter = new StringWriter();
+            Console.SetOut(stringwriter);
+
+            _uut.UnLockDoor();
+            Assert.AreEqual("Door is unlocked\r\n", stringwriter.ToString());
+        }
+
+        [Test]
+        public void TestLockDoor()
+        {
+            var stringwriter = new StringWriter();
+            Console.SetOut(stringwriter);
+
+            _uut.LockDoor();
+            Assert.AreEqual("Door is locked\r\n", stringwriter.ToString());
+        }
+
+        [Test]
+        public void TestOpenDoorEvent()
+        {
+            _uut.OpenDoor(true);
+            Assert.That(_receivedEventArgs,Is.Not.Null);
+        }
+
+        [Test]
+        public void TestCloseDoorEvent()
+        {
+            _uut.CloseDoor(false);
+            Assert.That(_receivedEventArgs, Is.Not.Null);
+        }
+
     }
 }
