11da08f9166af122f335d52661c74628dce588ea
katrine242 <katrinejepsen@gmail.com>
2022-03-22 10:37:03 +0100

    test til RFIDreader klassen

---
+++ ChargeStationUnitTests/TestRFIDreader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChargerStationGr8;
+using ChargeStationClassLibrary.RFIDReader;
+using NUnit.Framework;
+
+namespace ChargeStationUnitTests
+{
+   public class TestRFIDreader
+   {
+      private RFIReader _uut;
+      private RFIDChangedEventArgs _receivedEventArgs;
+
+      [SetUp]
+      public void Setup()
+      {
+         _receivedEventArgs = null;
+         _uut = new RFIReader();
+         _uut.IdChangedEvent += (o, args) => _receivedEventArgs = args;
+
+      }
+
+      [Test]
+      public void testReadIdIsCalledWithAnId()
+      {
+         _uut.ReadRFIDTag(1234);
+         Assert.That(_receivedEventArgs, Is.Not.Null);
+      }
+      [Test]
+
+      public void testReadId_IsCalled_WithTheRightId()
+      {
+         _uut.ReadRFIDTag(1234);
+         Assert.That(_receivedEventArgs.Id,Is.EqualTo(1234));
+      }
+   }
+}
