b0b3fd809ce467710f328255514124dba8146b26
katrine242 <katrinejepsen@gmail.com>
2022-03-22 15:32:32 +0100

    display

--- ChargeStationClassLibrary/StationControl.cs
+++ ChargeStationClassLibrary/StationControl.cs
@@ -59,7 +59,7 @@ namespace ChargeStationClassLibrary
                     }
                     else
                     {
-                        Console.WriteLine("Din telefon er ikke ordentlig tilsluttet. Prøv igen.");
+                        _display.PrintString("Din telefon er ikke ordentlig tilsluttet. Prøv igen.");
                     }
 
                     break;
@@ -77,12 +77,12 @@ namespace ChargeStationClassLibrary
                         _logFile.LogDoorUnlocked(id);
                         
 
-                    _display.PrintString("Tag din telefon ud af skabet og luk døren");
+                        _display.PrintString("Tag din telefon ud af skabet og luk døren");
                            _state = LadeskabState.Available;
                     }
                     else
                     {
-                        Console.WriteLine("Forkert RFID tag");
+                        _display.PrintString("Forkert RFID tag");
                     }
 
                     break;
