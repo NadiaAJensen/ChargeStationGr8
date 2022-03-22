d33a426d6a70506383e89a244286426a1998250b
Christina Jørgensen <au669829@uni.au.dk>
2022-03-22 10:22:08 +0100

    LogFileUnitTests

---
+++ ChargeStationUnitTests/LogFileUnitTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChargeStationClassLibrary;
+using ChargeStationClassLibrary.LogFile;
+using NUnit.Framework;
+
+namespace ChargeStationUnitTests
+{
+    [TestFixture]
+    public class LogFileUnitTest
+    {
+        private LogFile _uut;
+        private DTO_LogData _uutDtoLogData;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new LogFile();
+        }
+
+        [Test]
+       
+        public void CorrectId1234() // virker ikke
+        {
+            _uut.LogDoorLocked(1234);
+            Assert.That(_uutDtoLogData.Id, Is.EqualTo(1234));
+        }
+
+        [TestCase(1234)]
+        [TestCase(2345)]
+        [TestCase(3456)]
+        [TestCase(4567)]
+        [TestCase(5678)]
+        [TestCase(6789)]
+        [TestCase(7890)]
+
+        public void CorrectId(int id) //virker ikke
+        {
+            _uut.LogDoorLocked(id);
+            Assert.That(_uutDtoLogData.Id, Is.EqualTo(id));
+        }
+
+        [Test]
+        public void IsCalled_LockDoorLocked()
+        {
+            _uut.LogDoorLocked(1234);
+            Assert.That(_uutDtoLogData.Id, Is.EqualTo(1234));
+            Assert.That(_uutDtoLogData.Description, Is.EqualTo("Door is locked"));
+            Assert.That(_uutDtoLogData.Locked, Is.EqualTo(true));
+            Assert.That(_uutDtoLogData.TimeStamp, Is.EqualTo(DateTime.Now));
+        }
+
+        [Test]
+        public void IsCalled_UnlockDoorLocked()
+        {
+            _uut.LogDoorLocked(5678);
+            Assert.That(_uutDtoLogData.Id, Is.EqualTo(5678));
+            Assert.That(_uutDtoLogData.Description, Is.EqualTo("Door is unlocked"));
+            Assert.That(_uutDtoLogData.Locked, Is.EqualTo(false));
+            Assert.That(_uutDtoLogData.TimeStamp, Is.EqualTo(DateTime.Now));
+        }
+
+    }
+}
