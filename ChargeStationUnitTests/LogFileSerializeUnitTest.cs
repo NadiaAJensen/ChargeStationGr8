1eb9121dc75cd208260b86af2d693972782b4a72
Christina Jørgensen <au669829@uni.au.dk>
2022-03-22 11:30:01 +0100

    Udkommenteret Unit test for LogFileSerializeUnitTest

--- ChargeStationUnitTests/LogFileSerializeUnitTest.cs
+++ ChargeStationUnitTests/LogFileSerializeUnitTest.cs
@@ -4,28 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ChargeStationClassLibrary.LogFile;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace ChargeStationUnitTests
 {
-    public class LogFileSerializeUnitTest
+    public class LogFileSerializeUnitTest //skal denne klasse testes?
     {
-        private LogFileSerialize _uut;
-        private DTO_LogData _uutDtoLogData;
-        
+        //private LogFileSerialize _uut;
+        //private DTO_LogData _uutDtoLogData;
+        //private ILogFile _fakeLogFile;
 
-        [SetUp]
-        public void Setup()
-        {
-            _uut = new LogFileSerialize();
-            _uutDtoLogData = new DTO_LogData();
 
-        }
+        //[SetUp]
+        //public void Setup()
+        //{
+        //    _uut = new LogFileSerialize();
 
-        public void IsSaved()
-        {
-            //_uut.Save(_uutDtoLogData);
-            //Assert.That(_uut.Save(_uutDtoLogData), );
-        }
+        //}
+
+        //[Test]
+        //public void TestofDeserialization()
+        //{
+        //    var json = "{\"message\":\"Can You Read this message?\"}";
+        //    dynamic jsonResponse = JsonConvert.DeserializeObject(json);
+        //    dynamic d = JObject.Parse(json);
+        //    Assert.IsTrue(d.message.ToString() == "Can You Read this message?");
+        //    Assert.IsTrue(jsonResponse.message.ToString() == "Can You Read this message?");
+        //}
+        //[Test]
+        //public void TestofSerialization()
+        //{
+           
+        //}
+
     }
 }
