c3090e40d0fb4670b1c58dd4f84ceafeae61a18d
katrine242 <katrinejepsen@gmail.com>
2022-03-22 10:06:35 +0100

    test

--- ChargeStationUnitTests/TesRFIDreader.cs
+++ ChargeStationUnitTests/TesRFIDreader.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ChargerStationGr8;
+using ChargeStationClassLibrary.RFIDReader;
+using NUnit.Framework;
 
 namespace ChargeStationUnitTests
 {
-   class TesRFIDreader
+   public class TesRFIDreader
    {
+      private RFIReader _uut;
+      [SetUp]
+      public void Setup()
+      {
+         _uut = new RFIReader();
+      }
+
+      [Test]
+      public void test()
+      {
+
+      }
    }
 }
