150549cc9a726d0b2cc14a3a12796d43cbac850a
katrine242 <katrinejepsen@gmail.com>
2022-03-24 15:59:20 +0100

    test af stationcontrol

--- ChargeStationUnitTests/TestStationControl.cs
+++ ChargeStationUnitTests/TestStationControl.cs
@@ -104,6 +104,20 @@ namespace ChargeStationUnitTests
 
       }
 
+      [TestCase(1234, 1234, false)]
+
+      public void Testdoorclosed_oldIdIsNotNulld(int Old_id, int new_id, bool state)
+      {
+         _fakeChargeControl.Connected = true;
+         _fakeRfidReader.IdChangedEvent += Raise.EventWith(new RFIDChangedEventArgs { Id = Old_id });
+         _fakeRfidReader.IdChangedEvent += Raise.EventWith(new RFIDChangedEventArgs { Id = new_id });
+         _fakeDoor.DoorStatusChangedEvent += Raise.EventWith(new DoorChangedEventArgs { DoorStatus = false });
+
+
+         _fakeDisplay.Received(1).PrintString("Tag din telefon ud af skabet og luk døren");
+
+      }
+
 
    }
 }
